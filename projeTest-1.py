840c2ddcefffe15e683c91e7ec48e16e22636a99
Ümit Karadeniz <dkaradenzi12@gmail.com>
2024-12-02 12:03:11 +0300

    first code

---
+++ projeTest-1.py
@@ -0,0 +1,52 @@
+class Graph:
+    def __init__(self):  
+        self.graph = {}
+    
+    def add_city(self, city):
+        if city not in self.graph:
+            self.graph[city] = []
+    
+    def add_route(self, city1, city2, distance):
+
+        if city1 not in self.graph or city2 not in self.graph:
+            raise ValueError("Both cities must be added to the graph before adding a route.")
+        self.graph[city1].append((city2, distance))
+        self.graph[city2].append((city1, distance))
+    
+    def find_route(self, start, end):
+        if start not in self.graph or end not in self.graph:
+            raise ValueError("Both start and end cities must exist in the graph.")
+        
+        visited = set()
+        stack = [(start, [start])]
+        
+        while stack:
+            (city, path) = stack.pop()
+            if city not in visited:
+                visited.add(city)
+                
+                for (neighbor, _) in self.graph.get(city, []):
+                    if neighbor == end:
+                        return path + [end]
+                    else:
+                        stack.append((neighbor, path + [neighbor]))
+        return None
+
+
+g = Graph()
+g.add_city("Istanbul")
+g.add_city("Ankara")
+g.add_city("Izmir")
+g.add_city("Bursa")
+
+
+g.add_route("Istanbul", "Ankara", 450)
+g.add_route("Istanbul", "Bursa", 150)
+g.add_route("Bursa", "Izmir", 300)
+
+
+route = g.find_route("Istanbul", "Izmir")
+if route:
+    print("Rota bulundu:", " -> ".join(route))
+else:
+    print("Rota bulunamadı.")
